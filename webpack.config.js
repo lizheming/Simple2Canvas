e0de486fd88fe0037cb0acd1dab02a5d0cbb326e
lizheming <i@imnerd.org>
2020-02-23 13:00:41 +0800

    add webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const pkgName = 'simple2canvas';
+let filename = pkgName.toLowerCase() + '.umd';
+if (process.env.npm_lifecycle_script.includes('production')) {
+  filename += '.min';
+}
+filename += '.js';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename,
+    library: pkgName,
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader?cacheDirectory'
+      }
+    ]
+  }
+};
